facb79832799cc07bbbd3d4c3924163131070d87
Serghey <samsomskiff451@gmail.com>
2021-01-12 01:30:41 +0200

    trying to render table

--- script.js
+++ script.js
@@ -1,95 +1,5 @@
 'use strict';
 
-const obj = { a: 1, b: 2, c: 3 }
-
-console.log(Object.values(obj));
-
-
-function DataTable(config, data) {
-
-
-  if (!data) {
-    setServerDataIntoTable(config, constructTable);
-  } else {
-    constructTable(config, data);
-  }
-}
-
-function setServerDataIntoTable(config, tableConstructor) {
-  const url = config.apiUrl;
-  fetch(url)
-    .then((response) => response.json())
-    .then((json) => Object.values(json.data))
-    .then(tableData => {
-      console.log(tableData);
-      tableConstructor(config, tableData);
-    })
-}
-
-function constructTable(config, data) {
-  const parentElement = document.querySelector(config.parent),
-    columnsNames = config.columns.map(col => col.title),
-    columnsValues = config.columns.map(col => col.value);
-  const table = createTableElement('table', createTableHead(columnsNames))
-  table.append(createTableBody(columnsValues, data))
-  parentElement.append(table);
-}
-
-function createTableElement(elementName = 'td', content = 'null') {
-  const element = document.createElement(elementName);
-  if (Array.isArray(content)) {
-    content.forEach(item => element.append(item));
-  } else {
-    element.append(content);
-  }
-  return element;
-}
-
-function createTableHead(columnsNames) {
-  columnsNames.unshift('№');
-  const thArr = columnsNames.map(item => {
-    return createTableElement('th', item);
-  }),
-    trHead = createTableElement('tr', thArr);
-  return createTableElement('thead', trHead);
-}
-
-function createTableBody(columnsValues, data) {
-  const tableBodyTrs = [];
-  data.forEach((dataItem, index) => {
-    const tableBodyTds = [];
-    tableBodyTds.push(createTableElement('td', index + 1));
-    columnsValues.forEach(colProp => {
-
-      dataItem[colProp] = birthdayData(dataItem, colProp);
-
-      tableBodyTds.push(createTableElement('td', dataItem[colProp]));
-    })
-    tableBodyTrs.push(createTableElement('tr', tableBodyTds));
-  })
-  return createTableElement('tbody', tableBodyTrs);
-}
-
-function birthdayData(data, columnProperty) {
-  if (columnProperty === 'birthday') {
-    console.log('date1 ', data[columnProperty]);
-    const date = new Date(data[columnProperty]),
-      birthdayDay = `${addZero(date.getDay() + 1)}.${ addZero(date.getMonth() + 1)}.${date.getFullYear()}`;
-    return birthdayDay;
-  } else {
-    return data[columnProperty];
-  }
-}
-
-function addZero(dateItem) {
-  if (dateItem < 10) {
-    return `0${dateItem}`;
-  } else {
-    return dateItem;
-  }
-}
-
-
 const config1 = {
   parent: '#usersTable',
   apiUrl: 'http://mock-api.shpp.me/ssamohval/users/',
@@ -110,24 +20,178 @@ const users = [
 
 DataTable(config1);
 
+function DataTable(config, data) {
 
-// fetch('http://mock-api.shpp.me/ssamohval/users/1', {
+  if (!data) {
+    setServerDataIntoTable(config, constructTable);
+  } else {
+    constructTable(config, data);
+  }
+}
+
+function setServerDataIntoTable(config, tableConstructor) {
+  const url = config.apiUrl;
+  fetch(url)
+    .then((response) => response.json())// get response and transform to json
+    .then((json) => Object.values(json.data))// make into an array from big object "json.data"
+    .then(tableData => {
+      tableConstructor(config, tableData, url); //create table using server data which converted to array
+    })
+}
+
+//create table
+
+function constructTable(config, data, url) {
+  const parentElement = document.querySelector(config.parent),
+    columnsNames = config.columns.map(col => col.title),
+    columnsValues = config.columns.map(col => col.value);
+  const table = createTableElement('table', createTableHead(columnsNames))
+  table.append(createTableBody(columnsValues, data, url))
+  parentElement.append(table);
+}
+
+//create table element
+//'elementName' - a tag name where appends content and then returns it
+//'content' - can be a text, HTML element (array of HTML elements)
+
+function createTableElement(elementName = 'td', content = 'null') {
+  const element = document.createElement(elementName);
+  if (Array.isArray(content)) {
+    content.forEach(item => element.append(item));
+  } else {
+    element.append(content);
+  }
+  return element;
+}
+// create table head
+function createTableHead(columnsNames) {
+  columnsNames.unshift('№');
+  columnsNames.push('Действие');
+  const thArr = columnsNames.map(item => {
+    return createTableElement('th', item);
+  }),
+    trHead = createTableElement('tr', thArr);
+  return createTableElement('thead', trHead);
+}
+
+
+// create table body
+function createTableBody(columnsValues, data, url) {
+  const tableBodyTrs = [];
+  data.forEach((dataItem, index) => {
+    const tableBodyTds = [];
+    let indexId = index + 1;
+    tableBodyTds.push(createTableElement('td', indexId));
+    columnsValues.forEach(colProp => {
+      // checks if prop is birthday otherwise just returns without transformation
+      dataItem[colProp] = birthdayData(dataItem, colProp);
+      tableBodyTds.push(createTableElement('td', dataItem[colProp]));
+    })
+
+    
+    tableBodyTds.push(createTableElement('td', createDeleteBtn(indexId, url)));// add delete btn before adding a table row
+    tableBodyTrs.push(createTableElement('tr', tableBodyTds));
+  })
+  return createTableElement('tbody', tableBodyTrs);
+}
+
+function createDeleteBtn(id, url) {
+  const deleteBtn = document.createElement('btn');
+  deleteBtn.classList.add("delete-button");
+  deleteBtn.innerHTML = `<span>Удалить</span><img src="./icons/Remove icon.svg" alt="trash">`
+
+  deleteBtn.addEventListener('click', deleteFunction(id, url));
+  return deleteBtn;
+}
+
+function deleteFunction(id, url) {
+  return (event) => {
+    console.log(event.currentTarget);
+    console.log(id);
+    const deleteUrl = url + id;
+    fetch(deleteUrl,{
+      method: 'DELETE',
+    }).then();
+    // DataTable(config1)
+  }
+}
+
+// checks if data property is a date value
+// if it is true just transform value in a date readable 'string'
+// else returns property without transformation
+function birthdayData(data, columnProperty) {
+  if (columnProperty === 'birthday') {
+    const date = new Date(data[columnProperty]),
+      birthdayDay = `${addZero(date.getDay() + 1)}.${addZero(date.getMonth() + 1)}.${date.getFullYear()}`;
+    return birthdayDay;
+  } else {
+    return data[columnProperty];
+  }
+}
+
+
+
+// add zero to beautify date string
+function addZero(dateItem) {
+  if (dateItem < 10) {
+    return `0${dateItem}`;
+  } else {
+    return dateItem;
+  }
+}
+
+
+
+
+
+// fetch('http://mock-api.shpp.me/ssamohval/users/', {
 //   method: 'POST',
-//   body: JSON.stringify({
-//     "name": "Serghey",
-//     "surname": "Samokhval",
-//     "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/arashmanteghi/128.jpg",
-//     "birthday": "2020-12-24T13:42:31.357Z",
-//     "id": 1
-//   }),
+//   body: JSON.stringify(
+//     {
+//       "name": "Petr",
+//       "surname": "Sicor",
+//       "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/arashmanteghi/128.jpg",
+//       "birthday": "2021-12-24T13:42:31.357Z",
+//       "id": 1
+//     }
+
+//   ),
 //   headers: {
 //     'Content-type': 'application/json; charset=UTF-8',
 //   },
 // })
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
+//   .then((response) => response.text())
+//   .then((json) => {
+//     console.log("json in post", json)
+//   }).then(() => {
+//     fetch('http://mock-api.shpp.me/ssamohval/users')
+//       .then((response) => response.json())
+//       .then((json) => console.log(json));
+//   });
 
 
-// fetch('http://mock-api.shpp.me/ssamohval/users/1')
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
+
+
+  // fetch('http://mock-api.shpp.me/ssamohval/users/', {
+  //   method: 'POST',
+  //   body: JSON.stringify(
+  //     {
+  //       "name": "ZORRO",
+  //       "surname": "Zavalskii",
+  //       "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/arashmanteghi/123.jpg",
+  //       "birthday": "2021-01-24T13:42:31.357Z",
+  //     }
+
+  //   ),
+  //   headers: {
+  //     'Content-type': 'application/json; charset=UTF-8',
+  //   },
+  // })
+  //   .then((response) => response.text())
+  //   .then((json) => {
+  //     console.log("json in post", json)
+  //   }).then(() => {
+  //     fetch('http://mock-api.shpp.me/ssamohval/users')
+  //       .then((response) => response.json())
+  //       .then((json) => console.log(json));
+  //   });
